223fadef5a36251923c593a737b246bc5dc06fce
Douglas Gomes <douglasgommez@gmail.com>
2021-10-24 20:23:24 -0300

    start to implement MainActivity.kt

--- app/src/main/java/com/odougle/groceryapp/MainActivity.kt
+++ app/src/main/java/com/odougle/groceryapp/MainActivity.kt
@@ -3,36 +3,107 @@ package com.odougle.groceryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.odougle.groceryapp.ui.theme.GroceryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GroceryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MainTheme {
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainTheme(content: @Composable () -> Unit) {
+    GroceryAppTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GroceryAppTheme {
-        Greeting("Android")
+    MainTheme {
+        HomeScreen()
     }
 }
+
+@Composable
+fun HomeScreen() {
+    Box {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(0.dp, (-30).dp),
+            painter = painterResource(id = R.drawable.bg_main),
+            contentDescription = "Header Background",
+            contentScale = ContentScale.FillWidth
+        )
+        Scaffold(
+            topBar = { AppBar() },
+            backgroundColor = Color.Transparent
+        ) { paddingValues ->
+            Content(paddingValues)
+
+        }
+    }
+}
+
+@Composable
+fun AppBar() {
+    Row(
+        Modifier
+            .fillMaxHeight()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Search Food, Vegetable, etc.", fontSize = 12.sp) },
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "Search"
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+    }
+}
+
+@Composable
+fun Content(paddingValues: PaddingValues) {
+
+}
+
