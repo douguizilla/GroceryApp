4a5d717f30967d7918aa31e0cb8ad050f10482ee
Douglas Gomes <douglasgommez@gmail.com>
2021-10-27 19:44:49 -0300

    Add icons, add some composable functions.

--- app/src/main/java/com/odougle/groceryapp/MainActivity.kt
+++ app/src/main/java/com/odougle/groceryapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -103,10 +105,18 @@ fun AppBar() {
         )
         Spacer(modifier = Modifier.width(8.dp))
         IconButton(onClick = { }) {
-            Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = "", tint = Color.White)
+            Icon(
+                imageVector = Icons.Outlined.FavoriteBorder,
+                contentDescription = "",
+                tint = Color.White
+            )
         }
         IconButton(onClick = { }) {
-            Icon(imageVector = Icons.Outlined.Notifications, contentDescription = "", tint = Color.White)
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = "",
+                tint = Color.White
+            )
         }
 
 
@@ -115,7 +125,65 @@ fun AppBar() {
 
 @Composable
 fun Content(paddingValues: PaddingValues) {
-
+    Column(Modifier.padding(paddingValues)) {
+        Header()
+    }
 }
+
+@Composable
+fun Header() {
+    Card(
+        Modifier
+            .height(64.dp)
+            .padding(16.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            QrButton()
+            VerticalDivider()
+            Row (
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .clickable { }
+                    .padding(horizontal = 8.dp)
+                    ){
+                Icon(painter = painterResource(id = R.drawable.ic_money), contentDescription = "")
+            }
+        }
+    }
+}
+
+@Composable
+fun QrButton() {
+    IconButton(
+        onClick = {},
+        modifier = Modifier
+            .fillMaxHeight()
+            .aspectRatio(1f)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_scan),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun VerticalDivider() {
+    Divider(
+        color = Color(0xFFF1F1F1), modifier = Modifier
+            .width(1.dp)
+            .height(32.dp)
+    )
+}
+
 
 //video https://www.youtube.com/watch?v=pFZO5olP6HU&t=63 min 3:16
